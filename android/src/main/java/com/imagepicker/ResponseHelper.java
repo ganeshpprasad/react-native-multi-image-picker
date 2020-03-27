5310f77a5d2ecaa53659ab20259940fdb90cd4fa
ganeshp <ganeshpprasad@gmail.com>
2020-03-27 15:23:11 +0530

    multi image selector

--- android/src/main/java/com/imagepicker/ResponseHelper.java
+++ android/src/main/java/com/imagepicker/ResponseHelper.java
@@ -5,74 +5,71 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableArray;
+import java.util.ArrayList;
 
 /**
  * Created by rusfearuth on 24.02.17.
  */
 
-public class ResponseHelper
-{
+public class ResponseHelper {
     private WritableMap response = Arguments.createMap();
 
-    public void cleanResponse()
-    {
+    public void cleanResponse() {
         response = Arguments.createMap();
     }
 
-    public @NonNull WritableMap getResponse()
-    {
+    public @NonNull WritableMap getResponse() {
         return response;
     }
 
-    public void putString(@NonNull final String key,
-                          @NonNull final String value)
-    {
+    public void putString(@NonNull final String key, @NonNull final String value) {
         response.putString(key, value);
     }
 
-    public void putInt(@NonNull final String key,
-                       final int value)
-    {
+    public void putInt(@NonNull final String key, final int value) {
         response.putInt(key, value);
     }
 
-    public void putBoolean(@NonNull final String key,
-                           final boolean value)
-    {
+    public void putBoolean(@NonNull final String key, final boolean value) {
         response.putBoolean(key, value);
     }
 
-    public void putDouble(@NonNull final String key,
-                          final double value)
-    {
+    public void putDouble(@NonNull final String key, final double value) {
         response.putDouble(key, value);
     }
 
-    public void invokeCustomButton(@NonNull final Callback callback,
-                                   @NonNull final String action)
-    {
+    // TODO:
+    public void putArray(@NonNull final String key, final ArrayList<String> list) {
+        String[] returnArray = new String[list.size()];
+        returnArray = list.toArray(returnArray);
+
+        WritableArray promiseArray = Arguments.createArray();
+        for (int i = 0; i < returnArray.length; i++) {
+            promiseArray.pushString(returnArray[i]);
+        }
+        response.putArray(key, promiseArray);
+    }
+
+    public void invokeCustomButton(@NonNull final Callback callback, @NonNull final String action) {
         cleanResponse();
         response.putString("customButton", action);
         invokeResponse(callback);
     }
 
-    public void invokeCancel(@NonNull final Callback callback)
-    {
+    public void invokeCancel(@NonNull final Callback callback) {
         cleanResponse();
         response.putBoolean("didCancel", true);
         invokeResponse(callback);
     }
 
-    public void invokeError(@NonNull final Callback callback,
-                            @NonNull final String error)
-    {
+    public void invokeError(@NonNull final Callback callback, @NonNull final String error) {
         cleanResponse();
         response.putString("error", error);
         invokeResponse(callback);
     }
 
-    public void invokeResponse(@NonNull final Callback callback)
-    {
+    public void invokeResponse(@NonNull final Callback callback) {
         if (callback == null) {
             return;
         }
